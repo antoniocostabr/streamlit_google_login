5bff75f383cb839031b116e54aa7e9f2025eaf92
antoniocostabr <antoniocostabr@gmail.com>
2025-02-10 17:55:27 -0300

    Working app

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # streamlit_google_login
 Example of login using google account
+
+This is a simple Streamlit app for login using google account with the 
+login / logout components introduced in streamlit 1.42.0 / feb 2025
+
+
+## requirements
+
+- `streamlit==1.42.0`
+- `Authlib>=1.3.2`
+- `python-dotenv==1.0.1`
+- `Python 3.11.11`
+
+
+## secrets.toml
+```toml
+[auth]
+redirect_uri = "http://localhost:8501/oauth2callback"
+cookie_secret = "jkldhalskjgdasf7o39274jkbdjashvudasjk237898929387dkljahljkgh"
+
+[auth.google]
+client_id = "YOUR GOOGLE CLOUD AUTHENTICATION ID"
+client_secret = "YOUR GOOGLE CLOUD AUTHENTICATION SECRET"
+server_metadata_url = "https://accounts.google.com/.well-known/openid-configuration"
+```
+
+## enviroment variables
+
+```sh
+# list the domain allowed for login. If anyone with a google login account, specify "any"
+ALLOWED_DOMAIN=YOUR_ALLOWED_DOMAIN
+```
+
+## Usage
+
+```sh
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import time
+import os
+import dotenv
+
+
+# load the env variables / reading environment variables
+dotenv.load_dotenv()
+ALLOWED_DOMAIN = os.getenv('ALLOWED_DOMAIN')
+
+if not st.experimental_user.is_logged_in:
+    if st.button("Google Log in"):
+        st.login(provider="google")
+else:
+    if 'email' in st.experimental_user:
+        
+        # domain validation
+        if ALLOWED_DOMAIN != "any" and (not st.experimental_user.email.endswith(ALLOWED_DOMAIN)):
+            with st.spinner():
+                st.write(f'User email must be from {ALLOWED_DOMAIN}')
+                st.write(f'Logging out...')
+                time.sleep(3)
+            st.logout()
+        else:
+            st.image(st.experimental_user.get('picture'))
+            st.write(f"Hello, {st.experimental_user.name}")
+            st.write(f"email, {st.experimental_user.email}")
+            st.write('You are logged in!')
+            
+            if st.button("Log out"):
+                st.logout()
+
+            # this is where your app logic goes
+            
